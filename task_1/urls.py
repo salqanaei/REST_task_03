8d973cdb221636cfe1f39f48b2e40059f87a722b
Sulaiman AlQanaei <salqanaei@Sulaimans-MacBook-Air-2.local>
2019-07-24 18:22:58 +0300

    Final

--- task_1/urls.py
+++ task_1/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from flights import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +29,6 @@ urlpatterns = [
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
+    path('login/', TokenObtainPairView.as_view(), name="login"),
+    path('book/<int:booking_id>/', views.CreateBooking.as_view(), name="book-flight"),
 ]
